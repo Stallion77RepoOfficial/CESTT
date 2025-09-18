28de0441f160d976c7c569352e016730b3f7ecc0
Stallion77RepoOfficial <149940678+Stallion77RepoOfficial@users.noreply.github.com>
2025-09-18 19:29:08 +0300

    cleanup

--- engine_tests/multipv.py
+++
@@ -1,9 +0,0 @@
-from core import EngineRunner
-import chess
-
-
-def run(logger, report, path, movetime=0.25, multipv=4, instrumentation=None):
-    report.add_test("multipv", movetime=movetime, multipv=multipv)
-    with EngineRunner(path, multipv=multipv, instrumentation=instrumentation) as er:
-        cp, info = er.analyse_cp(chess.STARTING_FEN, movetime=movetime)
-        logger.log(f"[MultiPV] cp={cp} info_keys={list(info.keys())}")
